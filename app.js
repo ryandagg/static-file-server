411490ff8519ff816edc209e120e0d04847ec1ca
ryandagg <ryan.dagg@gmail.com>
2014-07-22 13:53:11 -0600

    changed fs from blocking to async with readFileSynce -> readFile.

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var fs = require('fs');
 
-var fileContents = fs.readFileSync('data.txt');
-
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.header("content-Type", "text/html");
-	res.render("index", {
-		data: fileContents
+// app.get('/', function(req, res) {
+// 	res.header("content-Type", "text/html");
+// 	res.render("index", {
+// 		data: fileContents
+// 	});
+// });
+
+var fileContents = fs.readFile('data.txt', function(err, str) {
+	app.get('/', function(req, res) {
+		res.header("content-Type", "text/html");
+		res.render("index", {
+			data: str
+		});
 	});
 });
 
